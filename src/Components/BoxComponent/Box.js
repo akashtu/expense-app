badb94a6b943a0cd64c9b51e0875a21444a743dc
Akash Yadav <akashyadav61234@gmail.com>
2024-03-26 10:59:28 +0530

    creating box

---
+++ src/Components/BoxComponent/Box.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Box.css";
+export const Box = () => {
+  return <div className="box-container"></div>;
+};
--- src/Components/ExpenseComponent/Expense.js
+++ src/Components/ExpenseComponent/Expense.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Expense.css";
 import { Header } from "../HeaderComponent/Header";
+import { Box } from "../BoxComponent/Box";
 export const Expense = () => {
   return (
     <div>
       <Header />
+      <Box />
     </div>
   );
 };
