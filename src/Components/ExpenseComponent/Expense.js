5ac1588d61aa07c4df5607e7938726aceb8ae05e
Akash Yadav <akashyadav61234@gmail.com>
2024-03-26 10:47:30 +0530

    creating three dots

---
+++ src/Components/ExpenseComponent/Expense.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Expense.css";
+import { Header } from "../HeaderComponent/Header";
+export const Expense = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
---
+++ src/Components/HeaderComponent/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+export const Header = () => {
+  return (
+    <div className="header-component">
+      <div className="header-dots">
+        <div className="dot dot1"></div>
+        <div className="dot dot2"></div>
+        <div className="dot dot3"></div>
+      </div>
+    </div>
+  );
+};
